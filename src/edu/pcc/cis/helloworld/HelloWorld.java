6bb61d67a60716a147ae8740941d200178d07b04
alka hatwal <alka.hatwal@pcc.edu>
2015-02-06 21:59:37 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Alka on 2/4/2017.
  */
@@ -7,5 +9,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        System.out.println(new Date());
     }
 }
