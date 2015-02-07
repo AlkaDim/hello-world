5cb22f9f2ff9c7ac16a4e9735899585e11bbe06f
alka hatwal <alka.hatwal@pcc.edu>
2015-02-06 21:44:55 -0800

    Added main method containing hello world message.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,5 +4,8 @@ package edu.pcc.cis.helloworld;
  * Created by Alka on 2/4/2017.
  */
 public class HelloWorld {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
 }
